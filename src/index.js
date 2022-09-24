50ce5c1e5f0ede23a2e6b2f4c9b6a2d056017dc3
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-09-24 22:59:47 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './components/Form'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import Form from "./components/Form";
+import Form2 from "./components/ ParentComponent";
 ReactDOM.render(
   <Form />,
-  document.getElementById('root')
+  <>
+    <Form2></Form2>
+  </>,
+
+  document.getElementById("root")
 );
